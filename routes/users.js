ffa1c14ae7d11be1494ded8518cb046057e18d35
cbartram <cbartram3@gmail.com>
2019-04-11 10:01:25 -0400

    Updated to reflect end of last video

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const { user } = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET /users
+router.get('/', (req, res, next) => {
+  user.findAll().then(users => res.json(users));
 });
 
 module.exports = router;
